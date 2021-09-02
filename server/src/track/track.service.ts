a2be9200a765ab974a18403dfc7a23f18e45f8a2
Mikhail <zemiza@yandex.ru>
2021-09-02 15:51:33 +0300

    first push

---
+++ server/src/track/track.service.ts
@@ -0,0 +1,47 @@
+import { FileService, FileType } from '../file/file.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { CreateTrackDto } from './dto/create-track.dto';
+import { Comment, CommentDocument } from './schemas/comment.schema';
+import { Track, TrackDocument } from './schemas/track.schema';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, ObjectId } from 'mongoose';
+
+@Injectable()
+export class TrackService {
+    constructor(
+        @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+        @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
+        private fileService: FileService) { }
+
+    async create(dto: CreateTrackDto, picture, audio): Promise<Track> {
+        const pathAudio = this.fileService.createFile(FileType.AUDIO, audio)
+        const pathPicture = this.fileService.createFile(FileType.IMAGE, picture)
+        const track = await this.trackModel.create({ ...dto, listens: 0, audio: pathAudio, picture: pathPicture });
+
+        return track;
+    }
+
+    async getAll(): Promise<Track[]> {
+        const tracks = await this.trackModel.find();
+        return tracks;
+    }
+
+    async getOne(id: ObjectId): Promise<Track> {
+        const track = await this.trackModel.findById(id).populate('comments')
+        return track;
+    }
+
+    async delete(id: ObjectId): Promise<ObjectId> {
+        const track = await this.trackModel.findByIdAndDelete(id)
+        return track._id;
+    }
+
+    async addComment(dto: CreateCommentDto): Promise<Comment> {
+        const track = await this.trackModel.findById(dto.trackId);
+        const comment = await this.commentModel.create({ ...dto })
+        track.comments.push(comment._id)
+        await track.save();
+        return comment;
+    }
+}
